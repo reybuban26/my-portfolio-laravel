aa49ab6b47ae9fc79d6fd9d5e40455b094f055a7
Rey Buban <reybuban11@gmail.com>
2026-03-12 17:01:02 +0800

    Add database setup route with migration handling
    Add a route to setup the database with migration.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChatbotController;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return view('welcome');
@@ -12,3 +13,12 @@ Route::get('/portfolio', function () {
 });
 
 Route::post('/api/chat', [ChatbotController::class, 'chat']);
+
+Route::get('/setup-db', function() {
+    try {
+        Artisan::call('migrate', ['--force' => true]);
+        return "Naka-pag migrate na ng database, Rey! Success!";
+    } catch (\Exception $e) {
+        return "May error sa database: " . $e->getMessage();
+    }
+});
